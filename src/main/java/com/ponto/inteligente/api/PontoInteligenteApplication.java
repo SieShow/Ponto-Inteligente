7e256416042262ad18cf91671cfba9f4dae7e11a
Lucas Gomes <lucasgsm88@gmail.com>
2018-01-04 13:35:45 -0200

    Criado bd e inserido migração com o flyway

--- src/main/java/com/ponto/inteligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/ponto/inteligente/api/PontoInteligenteApplication.java
@@ -2,7 +2,9 @@ package com.ponto.inteligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 @SpringBootConfiguration
 public class PontoInteligenteApplication {
 
