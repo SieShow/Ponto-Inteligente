2e452368d553ad4d40101508812523fcf86dfad2
Lucas Gomes <lucasgsm88@gmail.com>
2018-01-10 15:22:23 -0200

    Criado novo bd

---
+++ src/main/resources/db/migration/mysql/V2__admin_padrao.sql
@@ -0,0 +1,8 @@
+INSERT INTO `empresa` (`id`, `cnpj`, `data_atualizacao`, `data_criacao`, `razao_social`)
+VALUES (NULL, '82198127000121', CURRENT_DATE(), CURRENT_DATE(), 'Calangus IT');
+
+INSERT INTO `funcionario` (`id`, `cpf`, `data_atualizacao`, `data_criacao`, `email`, `nome`,
+`perfil`, `qtd_horas_almoco`, `qtd_horas_trabalho_dia`, `senha`, `valor_hora`, `empresa_id`)
+VALUES (NULL, '16248890935', CURRENT_DATE(), CURRENT_DATE(), 'admin@calangus.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL,
+'$2a$04$AAd28Va3vMF.vSsMuthiNublwh8jKXymC/BMnyh7L5vsfZI0DLZO2', NULL,
+(SELECT `id` FROM `empresa` WHERE `cnpj` = '82198127000121'));
