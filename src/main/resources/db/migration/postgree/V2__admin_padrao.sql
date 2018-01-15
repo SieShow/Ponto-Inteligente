4efdf67600be1d4efd8b8231988907e8c2228ba2
Lucas Gomes <lucasgsm88@gmail.com>
2018-01-15 17:06:11 -0200

    enviando sql para postgree

--- src/main/resources/db/migration/postgree/V2__admin_padrao.sql
+++ src/main/resources/db/migration/postgree/V2__admin_padrao.sql
@@ -1,8 +1,7 @@
-INSERT INTO empresa (id, cnpj, data_atualizacao, data_criacao, razao_social)
-VALUES (NULL, '82198127000121', CURRENT_DATE(), CURRENT_DATE(), 'Calangus IT');
+INSERT INTO empresa (cnpj, data_atualizacao, data_criacao, razao_social)
+VALUES ('82198127000121', current_timestamp, current_timestamp, 'Calangus IT');
 
-INSERT INTO funcionario (id, cpf, data_atualizacao, data_criacao, email, nome,
+INSERT INTO funcionario (cpf, data_atualizacao, data_criacao, email, nome,
 perfil, qtd_horas_almoco, qtd_horas_trabalho_dia, senha, valor_hora, empresa_id)
-VALUES (NULL, '16248890935', CURRENT_DATE(), CURRENT_DATE(), 'admin@calangus.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL,
-'$2a$04$AAd28Va3vMF.vSsMuthiNublwh8jKXymC/BMnyh7L5vsfZI0DLZO2', NULL,
-(SELECT id FROM empresa WHERE cnpj = '82198127000121'));
+VALUES ('16248890935', current_timestamp, current_timestamp, 'admin@calangus.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL,
+'$2a$04$AAd28Va3vMF.vSsMuthiNublwh8jKXymC/BMnyh7L5vsfZI0DLZO2', NULL, (SELECT id FROM empresa WHERE cnpj = '82198127000121'));
